50425947f6211ace583cfd0e211169eae0a68457
Thomas Naylor <thomasnaylor@outlook.com>
2019-01-07 00:48:36 +0200

    added function to build a 52 deck of cards

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,29 @@
 *  Thomas Naylor
 *  HTML, CSS, Bootstrap, JavaScript
 */
+
+(function() {
+  // global variable setup
+  const suits = ['spades', 'clubs', 'hearts', 'diamonds'];
+  const cards = ['ace','two','three', 'four', 'five', 'six',
+                 'seven', 'eight', 'nine', 'ten', 'jack',
+                 'queen', 'king'];
+
+  let deck = generateDeck();
+
+  // function to generate a new 52 desk of cards
+  function generateDeck() {
+    const data = [];
+
+    for(let i = 0; i < suits.length; i++) {
+      for(let j = 0; j < cards.length; j++) {
+        data.push(
+          { card: cards[j] + ' of ' + suits[i] }
+        );
+      }
+    }
+    return data;
+  }
+
+  // *WIP* - to be continued
+})();
